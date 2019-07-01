f5f978751418729811cd74f8933456096b7a3f92
wanjian <13717559259@163.com>
2019-07-01 19:38:37 +0800

    7-1

--- sp02-itemservice/src/main/java/com/tedu/sp02/item/controller/ItemController.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/item/controller/ItemController.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+//@ConfigurationProperties(value = "item-service-dev.yml")
 public class ItemController {
 	@Autowired
 	private ItemService itemService;
--- sp12-configcenter/src/main/java/com/tedu/sp12/Sp12ConfigApplication.java
+++ sp12-configcenter/src/main/java/com/tedu/sp12/Sp12ConfigApplication.java
@@ -1,18 +1,17 @@
-package com.tedu.configcenter;
+package com.tedu.sp12;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigServer
-public class ConfigcenterApplication {
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp12ConfigApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(ConfigcenterApplication.class, args);
+		SpringApplication.run(Sp12ConfigApplication.class, args);
 	}
 
 }
